34075baf926792ed0f909772c51763ec52a4cca8
Omar <omaar_5@hotmail.com>
2021-05-05 16:23:04 +0200

    fix: don't need sidebar anymore

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Background } from "./Background/Background";
 export { default as Navbar } from "./Navbar/Navbar";
-export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as SocialLinks } from "./SocialLinks/SocialLinks";
