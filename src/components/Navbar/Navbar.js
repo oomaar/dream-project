f60426324fda567d6ed2c7c6ed97301a387ba0b7
Omar <omaar_5@hotmail.com>
2021-05-05 16:19:08 +0200

    update: entire navbar logic

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,75 @@
-import { useState } from "react";
-import { Sidebar } from "..";
+// import { useState } from "react";
+// import { Sidebar } from "..";
+// import {
+//     Nav,
+//     Container,
+//     Signature,
+//     Span,
+//     MyName,
+//     Items,
+//     Item,
+//     div,
+//     NavBtn,
+// } from "./styledNavbar";
+
+// const Navbar = () => {
+//     const [toggleShow, setToggleShow] = useState(false);
+
+//     return (
+//         <Nav>
+// <Signature>
+//     <Span>&lt;</Span>
+//     <MyName>Omar Hassan</MyName>
+//     <Span>/&gt;</Span>
+// </Signature>
+//             <NavBtn onClick={() => setToggleShow(state => !state)}>
+//                 <i class="fas fa-bars"></i>
+//             </NavBtn>
+//             <Sidebar toggleShow={toggleShow} setToggleShow={setToggleShow} />
+//             <Container>
+//                 <Items>
+//                     <Item>
+//                         <div>Home</div>
+//                     </Item>
+//                     <Item>
+//                         <div>About</div>
+//                     </Item>
+//                     <Item>
+//                         <div>Education</div>
+//                     </Item>
+//                     <Item>
+//                         <div>Projects</div>
+//                     </Item>
+//                     <Item>
+//                         <div>Contact</div>
+//                     </Item>
+//                 </Items>
+//             </Container>
+//         </Nav>
+//     );
+// };
+
+// export default Navbar;
+
+// import { useState } from 'react';
+
 import {
     Nav,
-    Container,
+    Menu,
+    MenuItem,
+    MenuBtn,
+    MenuIcon,
+    NavIcon,
     Signature,
     Span,
     MyName,
-    Items,
-    Item,
-    Link,
-    NavBtn,
 } from "./styledNavbar";
 
 const Navbar = () => {
-    const [toggleShow, setToggleShow] = useState(false);
+    // const [nav, setNav] = useState(false);
+    // const changeBackground = () => window.scrollY >= 50 ? setNav(true) : setNav(false);
+    // window.addEventListener('scroll', changeBackground);
+    // <Nav className={nav && "active"}>
 
     return (
         <Nav>
@@ -22,29 +78,27 @@ const Navbar = () => {
                 <MyName>Omar Hassan</MyName>
                 <Span>/&gt;</Span>
             </Signature>
-            <NavBtn onClick={() => setToggleShow(state => !state)}>
-                <i class="fas fa-bars"></i>
-            </NavBtn>
-            <Sidebar toggleShow={toggleShow} setToggleShow={setToggleShow} />
-            <Container>
-                <Items>
-                    <Item>
-                        <Link>Home</Link>
-                    </Item>
-                    <Item>
-                        <Link>About</Link>
-                    </Item>
-                    <Item>
-                        <Link>Education</Link>
-                    </Item>
-                    <Item>
-                        <Link>Projects</Link>
-                    </Item>
-                    <Item>
-                        <Link>Contact</Link>
-                    </Item>
-                </Items>
-            </Container>
+            <MenuBtn id="menu-btn" type="checkbox" />
+            <MenuIcon htmlFor="menu-btn" for="menu-btn">
+                <NavIcon></NavIcon>
+            </MenuIcon>
+            <Menu>
+                <MenuItem>
+                    <a href="#">Home</a>
+                </MenuItem>
+                <MenuItem>
+                    <a href="#">About</a>
+                </MenuItem>
+                <MenuItem>
+                    <a href="#">Education</a>
+                </MenuItem>
+                <MenuItem>
+                    <a href="#">Projects</a>
+                </MenuItem>
+                <MenuItem>
+                    <a href="#">Contact</a>
+                </MenuItem>
+            </Menu>
         </Nav>
     );
 };
